94cb5694767245c6f8f80c8929ec22c496e81c74
ATulach <otulach@gmail.com>
2022-08-30 18:09:40 +0400

    add permute.js

---
+++ src/permute.js
@@ -0,0 +1,32 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+const permute = (nums) => {
+  const calc = (nums) => {
+    if (nums.length === 1) {
+      return [...nums];
+    }
+
+    let res = [];
+    for (let i = 0; i < nums.length; i++) {
+      let copy = [...nums];
+      copy.splice(i, 1);
+      let data = calc([...copy]);
+      for (let j = 0; j < data.length; j++) {
+        let item = data[j];
+        res.push(Array.isArray(item) ? [...item, nums[i]] : [item, nums[i]]);
+      }
+    }
+
+    return res;
+  };
+
+  let res = calc(nums);
+  if (res.length === 1) {
+    return [[...res]];
+  }
+  return res;
+};
+
+permute([1, 2, 3]);
