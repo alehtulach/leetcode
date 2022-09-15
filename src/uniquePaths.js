f478195f47648c7ee647b89d4111b828ede6a867
ATulach <otulach@gmail.com>
2022-09-15 22:09:18 +0400

    add uniquePaths.js

--- src/uniquePaths.js
+++ src/uniquePaths.js
@@ -22,4 +22,32 @@ const uniquePaths = (m, n) => {
   return row[0];
 };
 
-uniquePaths(3, 7);
+const uniquePaths1 = (m, n) => {
+  let cache = Array.from(Array(m), () => []);
+  const climb = (y, x) => {
+    let res1 = 0;
+    let res2 = 0;
+    if (y === m - 1 && x === n - 1) return 1;
+    if (y < m - 1) {
+      if (cache[y + 1][x]) {
+        res1 = cache[y + 1][x];
+      } else {
+        res1 = climb(y + 1, x);
+        cache[y + 1][x] = res1;
+      }
+    }
+    if (x < n - 1) {
+      if (cache[y][x + 1]) {
+        res2 = cache[y][x + 1];
+      } else {
+        res2 = climb(y, x + 1);
+        cache[y][x + 1] = res2;
+      }
+    }
+    return res1 + res2;
+  };
+
+  return climb(0, 0);
+};
+
+uniquePaths1(3, 7);
