e567ed81a55feaa5a9e6896c5f15498ec3b42ffa
ATulach <otulach@gmail.com>
2022-10-29 14:12:58 +0400

    add validateBinarySearchTree.js

---
+++ src/validateBinarySearchTree.js
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+const isValidBST = (root) => {
+  let isValid = true;
+  const dfs = (l, r, root) => {
+    if (!isValid || !root) return;
+    if (root.val < r && root.val > l) {
+      dfs(l, root.val, root.left);
+      dfs(root.val, r, root.right);
+    } else {
+      isValid = false;
+    }
+  };
+
+  dfs(Number.NEGATIVE_INFINITY, Number.POSITIVE_INFINITY, root);
+  return isValid;
+};
+
+function TreeNode(val, left, right) {
+  this.val = val === undefined ? 0 : val;
+  this.left = left === undefined ? null : left;
+  this.right = right === undefined ? null : right;
+}
+
+const createBT = (arr) => {
+  const create = (node, i) => {
+    if (node != null) {
+      if (2 * i + 1 < arr.length) {
+        if (arr[2 * i + 1] === null) {
+          node.left = null;
+        } else {
+          node.left = new TreeNode(arr[2 * i + 1]);
+          create(node.left, 2 * i + 1);
+        }
+      }
+      if (2 * i + 2 < arr.length) {
+        if (arr[2 * i + 2] === null) {
+          node.right = null;
+        } else {
+          node.right = new TreeNode(arr[2 * i + 2]);
+          create(node.right, 2 * i + 2);
+        }
+      }
+    }
+  };
+  const head = new TreeNode(arr[0]);
+  create(head, 0, arr);
+  return head;
+};
+
+isValidBST(createBT([5, 1, 4, null, null, 3, 6]));
